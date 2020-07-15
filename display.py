6adad21475f0bdcfe74aefae6f3240c6aa67bc81
minliang_mac <minliang.lin@hotstar.com>
2020-07-15 13:09:55 +0800

    add quotes

--- display.py
+++ display.py
@@ -3,19 +3,42 @@ import time
 import random
 import os
 
+def select(w: str) -> bool:
+    if len(w) > 5:
+        return False
+    if ' ' in w or '-' in w:
+        return False
+    ban_suffix = 'ness', 'ion', 'ian', 'ity', 'able'
+    for s in ban_suffix:
+        if w.endswith(s):
+            return False
+    return True
+
+
 def genFlash():
     with open('data/merge.json') as f:
-        j = json.load(f)
-    k = list(j.keys())
-    random.shuffle(k)
-    for t in k:
-        print('=' * 20)
+        dc = json.load(f)
+    with open('data/en_US_phon.txt') as f:
+        ph_dc = dict(s.split('\t') for s in f.readlines())
+
+    w_list = list(filter(select, dc.keys() & ph_dc.keys()))
+    random.shuffle(w_list)
+
+    for w in w_list:
         os.system('clear') # for macOS
-        print(t)
-        for m in j[t]['meanings']:
+        print(w, ph_dc[w])
+        for m in dc[w]['meanings'][:3]:
             for x in m:
-                if x != 'id':
-                    print('  ', x,':',  m[x])
+                if x == 'id':
+                    continue
+                y = m[x]
+                if isinstance(y, list) \
+                    and all(isinstance(z, str) for z in y):
+                    y = ', '.join(y)
+                if x == 'speech_part':
+                    x = 'type'
+                print(f'  {x}: {y}')
+            print()
         yield
 
 
@@ -25,6 +48,6 @@ if __name__ == '__main__':
             time.sleep(5 * 60)
         except:
             try:
-                time.sleep(3)
+                time.sleep(1)
             except:
                 print(), exit()
---
+++ quote.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from time import sleep
+from random import shuffle
+import random
+import os
+txt = pd.read_csv('data/quotes.csv').title
+shuffle(txt)
+w, h = os.get_terminal_size()
+c = h // 2
+for t in txt:
+    if not isinstance(t, str):
+        continue
+    os.system('clear')
+    print('\n'*c, t.center(w),'\n'*c)
+    try:
+        sleep(60 * 5)
+    except:
+        try:
+            sleep(1)
+        except:
+            exit()
+
